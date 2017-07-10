546b468f9cbb0c7976fb01c89a346d934dd8c3f4
Mikhail Koshmay <michail_koshmay@mail.ru>
2017-07-10 12:07:36 +0300

    add js

---
+++ script_1.js
@@ -0,0 +1,27 @@
+console.log("HI");
+
+angular.module("exampleApp", [])
+    .controller("defaultCtrl", function($scope) {
+        $scope.users = [{
+            name: "Table",
+            price: 44.10
+        }];
+
+
+        $scope.newUser = {
+            name,
+            type: "",
+            inputProfile,
+            inputLevel,
+            firstName,
+            lastName,
+            inputPassword,
+            inputEmail,
+            landPhone,
+            mobilePhone,
+            inputMap,
+            latitude: "",
+            longitude: ""
+        };
+
+    });

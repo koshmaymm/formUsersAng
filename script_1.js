d13fc8448a7c2878f50e941aee05baa222708723
Mikhail Koshmay <michail_koshmay@mail.ru>
2017-07-13 14:26:19 +0300

    change js

--- script_1.js
+++ script_1.js
@@ -1,27 +1,40 @@
-console.log("HI");
-
 angular.module("exampleApp", [])
-    .controller("defaultCtrl", function($scope) {
+    .controller("defaultCtrl", function($scope, $filter) {
+
         $scope.users = [{
             name: "Table",
             price: 44.10
         }];
 
+        $scope.noName = true;
 
-        $scope.newUser = {
-            name,
-            type: "",
-            inputProfile,
-            inputLevel,
-            firstName,
-            lastName,
-            inputPassword,
-            inputEmail,
-            landPhone,
-            mobilePhone,
-            inputMap,
-            latitude: "",
-            longitude: ""
+        $scope.newUser = function() {
+            $scope.name = $scope.inputUserAccount,
+                $scope.type = $scope.inputName,
+                $scope.inputProfile,
+                $scope.inputLevel,
+                $scope.firstName,
+                $scope.lastName,
+                $scope.inputPassword,
+                $scope.inputEmail,
+                $scope.landPhone,
+                $scope.mobilePhone,
+                $scope.inputMap,
+                $scope.latitude,
+                $scope.longitude;
         };
 
+        $scope.showName = function() {
+
+            if ($scope.noName == true) {
+                $scope.noName = false;
+            } else {
+                $scope.noName = true;
+            }
+            console.log($scope.noName);
+        }
+
+        $scope.tryAddNewUser = function() {
+            console.log("<<<=====>>>");
+        }
     });

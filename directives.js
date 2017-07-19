c895a49058557df5afb125696fb3d0230ebecd3f
Mikhail Koshmay <michail_koshmay@mail.ru>
2017-07-19 17:02:44 +0300

    add directives

---
+++ directives.js
@@ -0,0 +1,49 @@
+angular.module("exampleApp")
+    .directive("hightlight", function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element) {
+
+                element.on("mouseover", function(event) {
+                    angular.element(element).css({ color: scope.color })
+
+                })
+                element.on("mouseout", function(event) {
+                    angular.element(element).css({ color: "black" })
+                })
+            },
+            scope: { color: "@" }
+        };
+    })
+    .directive("strongfont", function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element) {
+
+                element.on("mouseover", function(event) {
+                    angular.element(element).css({ fontWeight: "bold" })
+
+                })
+                element.on("mouseout", function(event) {
+                    angular.element(element).css({ fontWeight: "normal" })
+                })
+            },
+            scope: { fontWeight: "@" }
+        };
+    })
+    .directive("strongborder", function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element) {
+
+                element.on("mouseenter", function(event) {
+                    angular.element(element).css({ border: "3px outset gold" })
+
+                })
+                element.on("mouseout", function(event) {
+                    angular.element(element).css({ border: scope.border })
+                })
+            },
+            scope: { border: "@" }
+        };
+    })
